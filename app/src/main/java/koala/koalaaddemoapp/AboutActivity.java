9bb9fc9121a1e3a711e73803c24e83e8e3024097
leotse90 <proleo@foxmail.com>
2016-07-30 14:40:21 +0800

    optimize about-me UI and add more details for sdk

--- app/src/main/java/koala/koalaaddemoapp/AboutActivity.java
+++ app/src/main/java/koala/koalaaddemoapp/AboutActivity.java
@@ -1,14 +1,16 @@
 package koala.koalaaddemoapp;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Window;
 import android.widget.TextView;
 
 /**
  * Created by leotse on 16/7/28.
  */
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends AppCompatActivity {
 
     private TextView mAboutTxt;
 
@@ -23,6 +25,16 @@ public class AboutActivity extends Activity {
     private void initWidget(){
         mAboutTxt = (TextView) findViewById(R.id.about_txt);
 
-        mAboutTxt.setText("Koala SDK\nKoala SDK支持原生广告、横幅广告、插屏广告以及视频广告。");
+        String html = "<font color='#86B404'><strong>功能简介</strong></font><br>";
+        html += "<font>Koala SDK由Koala广告平台开发,提供了原生广告、横幅广告、插屏广告以及视频广告等丰富的广告形式。<br>" +
+                "Koala平台广告资源覆盖全球240+国家和地区,并强力聚合了Facebook Audience Network和Admob广告,使用非常便捷," +
+                "一小时内可以完成接入。<br>" +
+                "在使用Koala广告资源前,你需要注册平台账号以及申请广告id,并获取最新的广告SDK。</font><br><br>";
+
+        html += "<font color='#86B404'><strong>联系我们</strong></font><br>";
+        html += "<font>QQ:1479348217</font><br>";
+        html += "<font>Email:februus90@gmail.com</font>";
+
+        mAboutTxt.setText(Html.fromHtml(html));
     }
 }
--- app/src/main/java/koala/koalaaddemoapp/BannerAdActivity.java
+++ app/src/main/java/koala/koalaaddemoapp/BannerAdActivity.java
@@ -2,7 +2,6 @@ package koala.koalaaddemoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
