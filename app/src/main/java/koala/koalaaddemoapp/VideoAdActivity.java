f4424b69d353e99e5a54fdaa6d763c4d27a1865b
leotse90 <proleo@foxmail.com>
2016-08-01 16:54:26 +0800

    destroy video ad when necessary

--- app/src/main/java/koala/koalaaddemoapp/VideoAdActivity.java
+++ app/src/main/java/koala/koalaaddemoapp/VideoAdActivity.java
@@ -23,12 +23,23 @@ public class VideoAdActivity extends Activity {
     private Button mLoadVideoBtn;
     private ProgressBar mProgressBar;
 
+    private KoalaVideoAdView mKoalaVideoAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_layout);
 
         initWidget();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // destroy video ad
+        if (mKoalaVideoAdView != null){
+            mKoalaVideoAdView.destroyVideoView();
+        }
     }
 
     private void initWidget(){
@@ -47,8 +58,8 @@ public class VideoAdActivity extends Activity {
 
     private void loadVideoAd(){
         ADFactory.ADRequestSetting adRequestSetting = ADFactory.getADRequestSetting(KoalaAdDemoConstants.VIDEO_AD_OID);
-        KoalaVideoAdView koalaVideoAdView = (KoalaVideoAdView) findViewById(R.id.koala_video_ad);
-        koalaVideoAdView.loadISVideoAd(adRequestSetting, new NativeAdListener.RequestVideoAdListener() {
+        mKoalaVideoAdView = (KoalaVideoAdView) findViewById(R.id.koala_video_ad);
+        mKoalaVideoAdView.loadISVideoAd(adRequestSetting, new NativeAdListener.RequestVideoAdListener() {
             @Override
             public void onSuccess() {
                 Log.d(KoalaAdDemoConstants.LOG_TAG, "video ad load succeed");
@@ -72,6 +83,8 @@ public class VideoAdActivity extends Activity {
             @Override
             public void onComplete() {
                 Log.d(KoalaAdDemoConstants.LOG_TAG, "video ad play completed");
+                // destroy video ad view when necessary
+                mKoalaVideoAdView.destroyVideoView();
                 Toast.makeText(VideoAdActivity.this, "视频广告播放完毕", Toast.LENGTH_SHORT).show();
             }
 
